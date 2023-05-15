ec0144fb9466c5ec16e5f8b63432cf62befd4c3e
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-15 19:37:50 +0800

    Tugas Array

---
+++ src/main/java/array/TugasArray.java
@@ -0,0 +1,26 @@
+package array;
+
+public class TugasArray {
+    public static void main(String[] args) {
+        String jadwal[];
+        jadwal = new String[8];
+        
+        System.out.println("Jadwal Perkuliahan UNISKA Semester 4");
+        System.out.println("====================================");
+        jadwal[0] = "Senin = Sistem Terdistribusi & Pemrograman Web 2";
+        jadwal[1] = "Selasa = Perancangan Database & Pemrograman Visual 2";
+        jadwal[2] = "Rabu = Pemrograman Berbasis Objek & Filsafat";
+        jadwal[3] = "Kamis = Sistem Informasi Geografis & Jaringan Komputer 2";
+        jadwal[4] = "Jumat = Sosial Budaya & Tauhid";
+        jadwal[5] = "Sabtu = Mabar";
+        jadwal[6] = "Minggu = Mabar";
+        
+        System.out.println(jadwal[0]);
+        System.out.println(jadwal[1]);
+        System.out.println(jadwal[2]);
+        System.out.println(jadwal[3]);
+        System.out.println(jadwal[4]);
+        System.out.println(jadwal[5]);
+        System.out.println(jadwal[6]);
+    }
+}
