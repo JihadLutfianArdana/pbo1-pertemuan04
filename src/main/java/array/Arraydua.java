f176c1422090a1949d45fce8f5c73b4eab9da397
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-15 11:19:50 +0800

    Array2

---
+++ src/main/java/array/Arraydua.java
@@ -0,0 +1,20 @@
+package array;
+
+public class Arraydua {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+        
+        mataKuliah[0] = "PBO 1";
+        mataKuliah[1] = "WEB 2";
+        mataKuliah[2] = "Organisasi Komputer";
+        mataKuliah[3] = "Sistem Basis Data 2";
+        mataKuliah[4] = "Logika Matematika";
+        
+        System.out.println(mataKuliah[0]);
+        System.out.println(mataKuliah[1]);
+        System.out.println(mataKuliah[2]);
+        System.out.println(mataKuliah[3]);
+        System.out.println(mataKuliah[4]);
+    }
+}
