92e5cf39d9f97263bbba8d868c2d6d3fc779dedd
JihadLutfianArdana <lutfianjihadjizy@gmail.com>
2023-05-15 11:13:11 +0800

    Array1

---
+++ src/main/java/array/Arraysatu.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Arraysatu {
+    public static void main(String[] args) {
+        int nilaiArr[] = {10,20,30,40,50,60,70,80,90,100};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[5]);
+        System.out.println(nilaiArr[9]);
+    }
+}
